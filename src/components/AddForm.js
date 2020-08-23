ef02d75adf2ae88a1cfe83e055ddaabd7dd9d7c6
Manish Poduval <manishpoduval@hotmail.com>
2020-08-23 12:38:20 +0530

    Update AddForm.js

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -8,6 +8,10 @@ export default function AddForm(props) {
         <form onSubmit={props.onSubmit}>
             <input name="name" type="text" placeholder="Enter Name"></input>
             <input name="description" type="text" placeholder="Enter Description"></input>
+           {/*<div className="form-group">
+                    <label htmlFor="image">Image</label>
+                    <input type="file" className="form-control" name="image" id="image" />
+            </div> */}
             <button type="submit">Create</button>
         </form>
     )
